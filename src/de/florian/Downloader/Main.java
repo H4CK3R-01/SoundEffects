5231995e7e6f45de4b84e80777e8299706cbb632
H4CK3R-01 <H4CK3R-01@users.noreply.github.com>
2019-05-03 22:07:04 +0200

    Added Files

---
+++ src/de/florian/Downloader/Main.java
@@ -0,0 +1,71 @@
+package de.florian.Downloader;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		String line;
+		HashSet<String> list = new HashSet<>();
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader("BBCSoundEffects.csv"));
+			while ((line = br.readLine()) != null) {
+				list.add(line);
+			}
+			br.close();
+		} catch (IOException e1) {
+			System.out.println("Test");
+		}
+
+		Iterator<String> iterator = list.iterator();
+		int i = 0;
+		while (iterator.hasNext()) {
+			i++;
+			String[] splitted = iterator.next().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+			String nummer = splitted[0].replaceAll("[^A-Za-z0-9\\s.]", "").replaceAll("\\s+", " ").trim();
+			String beschreibung = splitted[1].replaceAll("[^A-Za-z0-9\\s.]", "").replaceAll("\\s+", " ").trim();
+			String album = splitted[5].replaceAll("[^A-Za-z0-9\\s.]", "").replaceAll("\\s+", " ").trim();
+
+			System.out.println(i + ": " + "D:\\Test\\" + album + "\\" + nummer.replace(".wav", "") + " - "
+					+ beschreibung + ".wav");
+
+			File file = new File("D:\\Test\\" + album + "\\" + nummer.replace(".wav", "") + " - " + beschreibung + ".wav");
+			if (!file.exists()) {
+				try {
+					Path filePath = Paths.get("D:\\Test\\" + album + "\\" + nummer.replace(".wav", "") + " - " + beschreibung + ".wav");
+					Files.createDirectories(filePath.getParent());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				final URL url = new URL("http://bbcsfx.acropolis.org.uk/assets/" + nummer);
+				final URLConnection conn = url.openConnection();
+				final InputStream is = new BufferedInputStream(conn.getInputStream());
+				final OutputStream os = new BufferedOutputStream(new FileOutputStream("D:\\Test\\" + album + "\\" + nummer.replace(".wav", "") + " - " + beschreibung + ".wav"));
+				byte[] chunk = new byte[1024];
+				int chunkSize;
+				while ((chunkSize = is.read(chunk)) != -1) {
+					os.write(chunk, 0, chunkSize);
+				}
+				os.close();
+				is.close();
+			}
+		}
+	}
+}
