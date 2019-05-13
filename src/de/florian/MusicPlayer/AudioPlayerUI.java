e55e6354268e3c855f43921b4bfedcb66255014d
H4CK3R-01 <H4CK3R-01@users.noreply.github.com>
2019-05-13 13:23:37 +0200

    Added MusicPlayer and menubar

---
+++ src/de/florian/MusicPlayer/AudioPlayerUI.java
@@ -0,0 +1,209 @@
+package de.florian.MusicPlayer;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+public class AudioPlayerUI extends JFrame implements ActionListener {
+    private AudioPlayer player = new AudioPlayer();
+    private Thread playbackThread;
+    private PlayingTimer timer;
+
+    private boolean isPlaying = false;
+    private boolean isPause = false;
+
+    private String audioFilePath;
+
+    private JLabel labelFileName = new JLabel("Datei: ");
+    private JLabel labelTimeCounter = new JLabel("00:00:00");
+    private JLabel labelDuration = new JLabel("00:00:00");
+
+    private JButton buttonPlay = new JButton("Play");
+    private JButton buttonPause = new JButton("Pause");
+
+    private JSlider sliderTime = new JSlider();
+
+
+    public AudioPlayerUI() {
+        super("SoundEffects - Player");
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(5, 5, 5, 5);
+        constraints.anchor = GridBagConstraints.WEST;
+
+        buttonPlay.setFont(new Font("Sans", Font.BOLD, 14));
+        buttonPlay.setEnabled(false);
+
+        buttonPause.setFont(new Font("Sans", Font.BOLD, 14));
+        buttonPause.setEnabled(false);
+
+        labelTimeCounter.setFont(new Font("Sans", Font.BOLD, 12));
+        labelDuration.setFont(new Font("Sans", Font.BOLD, 12));
+
+        sliderTime.setPreferredSize(new Dimension(400, 20));
+        sliderTime.setEnabled(false);
+        sliderTime.setValue(0);
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 3;
+        add(labelFileName, constraints);
+
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        add(labelTimeCounter, constraints);
+
+        constraints.gridx = 1;
+        add(sliderTime, constraints);
+
+        constraints.gridx = 2;
+        add(labelDuration, constraints);
+
+        JPanel panelButtons = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 5));
+        panelButtons.add(buttonPlay);
+        panelButtons.add(buttonPause);
+
+        constraints.gridwidth = 3;
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        add(panelButtons, constraints);
+
+        buttonPlay.addActionListener(this);
+        buttonPause.addActionListener(this);
+
+        pack();
+        setResizable(false);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent evt) {
+                stopPlaying();
+                setVisible(false);
+            }
+        });
+        setLocationRelativeTo(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        Object source = event.getSource();
+        if (source instanceof JButton) {
+            JButton button = (JButton) source;
+			/*if (button == buttonOpen) {
+				openFile();
+			} else*/
+            if (button == buttonPlay) {
+                if (!isPlaying) {
+                    playBack();
+                } else {
+                    stopPlaying();
+                }
+            } else if (button == buttonPause) {
+                if (!isPause) {
+                    pausePlaying();
+                } else {
+                    resumePlaying();
+                }
+            }
+        }
+    }
+
+
+    public void openFile(String path) {
+        audioFilePath = path;
+        if (isPlaying || isPause) {
+            stopPlaying();
+            while (player.getAudioClip().isRunning()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                }
+            }
+        }
+        playBack();
+    }
+
+    private void playBack() {
+        timer = new PlayingTimer(labelTimeCounter, sliderTime);
+        timer.start();
+        isPlaying = true;
+        playbackThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    buttonPlay.setText("Stop");
+                    buttonPlay.setEnabled(true);
+
+                    buttonPause.setText("Pause");
+                    buttonPause.setEnabled(true);
+
+                    player.load(audioFilePath);
+                    timer.setAudioClip(player.getAudioClip());
+                    labelFileName.setText("Datei: " + audioFilePath);
+                    sliderTime.setMaximum((int) player.getClipSecondLength());
+
+                    labelDuration.setText(player.getClipLengthString());
+                    player.play();
+
+                    resetControls();
+                } catch (UnsupportedAudioFileException ex) {
+                    JOptionPane.showMessageDialog(AudioPlayerUI.this, "Format wird nicht unterstützt!", "Error", JOptionPane.ERROR_MESSAGE);
+                    resetControls();
+                    ex.printStackTrace();
+                } catch (LineUnavailableException ex) {
+                    JOptionPane.showMessageDialog(AudioPlayerUI.this, "Datei kann nicht abgespielt werden!", "Error", JOptionPane.ERROR_MESSAGE);
+                    resetControls();
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(AudioPlayerUI.this, "I/O Fehler!", "Error", JOptionPane.ERROR_MESSAGE);
+                    resetControls();
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+
+        playbackThread.start();
+    }
+
+    private void stopPlaying() {
+        isPause = false;
+        buttonPause.setText("Pause");
+        buttonPause.setEnabled(false);
+        timer.reset();
+        timer.interrupt();
+        player.stop();
+        playbackThread.interrupt();
+    }
+
+    private void pausePlaying() {
+        buttonPause.setText("Play");
+        isPause = true;
+        player.pause();
+        timer.pauseTimer();
+        playbackThread.interrupt();
+    }
+
+    private void resumePlaying() {
+        buttonPause.setText("Pause");
+        isPause = false;
+        player.resume();
+        timer.resumeTimer();
+        playbackThread.interrupt();
+    }
+
+    private void resetControls() {
+        timer.reset();
+        timer.interrupt();
+
+        buttonPlay.setText("Play");
+        buttonPause.setEnabled(false);
+        isPlaying = false;
+    }
+}
--- src/de/florian/Search/UI.java
+++ src/de/florian/Search/UI.java
@@ -1,97 +1,42 @@
 package de.florian.Search;
 
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import de.florian.MusicPlayer.AudioPlayerUI;
+
 import javax.swing.*;
+import javax.swing.table.TableColumn;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.File;
+import java.awt.event.*;
 import java.sql.*;
 
 public class UI extends Thread {
     private final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
-    private final String DB_URL = "jdbc:mariadb://localhost/SoundEffects";
-    private final String USER = "pi";
-    private final String PASS = "Admin!123";
+    private String url = "jdbc:mariadb://localhost/SoundEffects";
+    private String user = "pi";
+    private String pass = "Admin!123";
 
-    static JFrame mainFrame;
-    static PlaceholderTextField searchTextField;
-    static JScrollPane tableScrollPane;
-    static JTable table;
+    private String path = "/media/florian/DATA/BBCSoundEffects/";
+
+    private JFrame mainFrame;
+    private JMenuBar menuBar;
+    private JMenu file;
+    private JMenuItem changeDirectory, changeDBurl, changeUser, changePass;
+    private JFileChooser chooser;
+    private PlaceholderTextField searchTextField;
+    private JScrollPane tableScrollPane;
+    private JTable table;
 
     private String[][] data;
     private String[] columnNames = {"Beschreibung", "Dauer", "Dateiname"};
 
-
     private Connection connection = null;
     private Statement statement = null;
     private ResultSet resultSet = null;
-
-    public UI() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-        }
-    }
-
-    public void run() {
-        mainFrame = new JFrame("SoundEffects - Search");
-        searchTextField = new PlaceholderTextField("");
-        searchTextField.setPlaceholder("Suche ...");
-
-        searchTextField.addActionListener(actionListener);
-        mainFrame.add(searchTextField, BorderLayout.NORTH);
-
-        table = new JTable(data, columnNames) {
-            public boolean isCellEditable(int x, int y) {
-                return false;
-            }
-        };
-        table.setRowHeight(30);
-        table.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                System.out.println("1");
-                if (e.getClickCount() == 2) {
-                    System.out.println("2");
-                    JTable target = (JTable) e.getSource();
-                    int row = target.getSelectedRow();
-
-                    File sound = new File("/media/florian/DATA/BBCSoundEffects/" + table.getValueAt(row, 2).toString());
-
-                    try
-                    {
-                        Clip clip = AudioSystem.getClip();
-                        clip.open(AudioSystem.getAudioInputStream(sound));
-                        clip.start();
-                    }
-                    catch (Exception exc)
-                    {
-                        exc.printStackTrace(System.out);
-                    }
-                }
-            }
-        });
-
-        tableScrollPane = new JScrollPane(table);
-        mainFrame.add(tableScrollPane, BorderLayout.CENTER);
-
-        mainFrame.setSize(new Dimension(1600, 900));
-        mainFrame.setLocationRelativeTo(null);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        data = new String[1][3];
-        paintFrame();
-    }
-
     ActionListener actionListener = new ActionListener() {
         public void actionPerformed(ActionEvent e) {
             try {
                 table = null;
                 Class.forName(JDBC_DRIVER);
-                connection = DriverManager.getConnection(DB_URL, USER, PASS);
+                connection = DriverManager.getConnection(url, user, pass);
                 statement = connection.createStatement();
 
                 resultSet = statement.executeQuery("select * from Effects WHERE Beschreibung LIKE '%" + searchTextField.getText() + "%';");
@@ -102,6 +47,7 @@ public class UI extends Thread {
                     rowSize = resultSet.getRow();
                     resultSet.beforeFirst();
                 } catch (Exception ex) {
+                    System.err.println(ex);
                 }
 
                 data = new String[rowSize + 1][3];
@@ -112,32 +58,101 @@ public class UI extends Thread {
                 }
                 paintFrame();
             } catch (SQLException se) {
+                System.err.println(se);
             } catch (Exception ex) {
+                System.err.println(ex);
             } finally {
                 try {
                     if (statement != null) {
                         connection.close();
                     }
                 } catch (SQLException se) {
+                    System.err.println(se);
                 }
                 try {
                     if (connection != null) {
                         connection.close();
                     }
                 } catch (SQLException se) {
+                    System.err.println(se);
                 }
             }
         }
     };
 
-
-    private void paintFrame() {
-        mainFrame.setVisible(false);
+    public UI() {
         try {
-            mainFrame.remove(tableScrollPane);
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println(e);
         }
+    }
+
+    public void run() {
+        mainFrame = new JFrame("SoundEffects - Search");
+
+        menuBar = new JMenuBar();
+        file = new JMenu("Datei");
+
+        changeDirectory = new JMenuItem("Verzeichnis ändern");
+        changeDirectory.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                chooser = new JFileChooser();
+                chooser.setCurrentDirectory(new java.io.File("."));
+                chooser.setDialogTitle("Verzeichnis der Soundeffekte auswählen");
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                chooser.setAcceptAllFileFilterUsed(false);
+                if (chooser.showOpenDialog(mainFrame) == JFileChooser.APPROVE_OPTION) {
+                    path = chooser.getSelectedFile().getAbsolutePath() + "/";
+                    System.out.println(path);
+                }
+            }
+        });
+
+        changeDBurl = new JMenuItem("Datenbank-URL ändern");
+        changeDBurl.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                url = JOptionPane.showInputDialog("URL der Datenbank ändern (Format: jdbc:Datenbanksystem://Server/Datenbankname) \nStandard ist: jdbc:mariadb://localhost/SoundEffects");
+            }
+        });
+
+        changeUser = new JMenuItem("Benutzer ändern");
+        changeUser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                user = JOptionPane.showInputDialog("Benutzer der Datenbank ändern");
+                System.out.println(user);
+            }
+        });
+
+        changePass = new JMenuItem("Passwort ändern");
+        changePass.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JPasswordField passwordField = new JPasswordField();
+                passwordField.setEchoChar('*');
+                JOptionPane.showMessageDialog(null, passwordField, "Passwort des Datenbank-Benutzers ändern", JOptionPane.OK_OPTION);
+                pass = String.valueOf(passwordField.getPassword());
+                System.out.println(pass);
+            }
+        });
+
+        file.add(changeDirectory);
+        file.addSeparator();
+        file.add(changeDBurl);
+        file.add(changeUser);
+        file.add(changePass);
+
+        menuBar.add(file);
+
+        mainFrame.setJMenuBar(menuBar);
+
+        searchTextField = new PlaceholderTextField("");
+        searchTextField.setPlaceholder("Suche ...");
+        searchTextField.addActionListener(actionListener);
+        mainFrame.add(searchTextField, BorderLayout.NORTH);
 
         table = new JTable(data, columnNames) {
             public boolean isCellEditable(int x, int y) {
@@ -145,25 +160,64 @@ public class UI extends Thread {
             }
         };
 
+        tableScrollPane = new JScrollPane(table);
+        mainFrame.add(tableScrollPane, BorderLayout.CENTER);
+
+        mainFrame.setSize(new Dimension(1600, 900));
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent componentEvent) {
+                setColumnWidth();
+            }
+        });
+
+        data = new String[1][3];
+        paintFrame();
+    }
+
+    private void paintFrame() {
+        mainFrame.setVisible(false);
+        try {
+            mainFrame.remove(tableScrollPane);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+        table = new JTable(data, columnNames) {
+            public boolean isCellEditable(int x, int y) {
+                return false;
+            }
+        };
         table.setRowHeight(30);
         table.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 if (e.getClickCount() == 2) {
                     JTable target = (JTable) e.getSource();
-                    int row = target.getSelectedRow();
 
-                    File sound = new File("/media/florian/DATA/BBCSoundEffects/" + table.getValueAt(row, 2).toString());
-
-                    new MusicPlayer(table.getValueAt(row, 0).toString(), sound);
-                    //new AudioPlayer(sound);
+                    AudioPlayerUI player = new AudioPlayerUI();
+                    player.setVisible(true);
+                    player.openFile(path + table.getValueAt(target.getSelectedRow(), 2).toString());
                 }
             }
         });
+        setColumnWidth();
         tableScrollPane = new JScrollPane(table);
         mainFrame.add(tableScrollPane, BorderLayout.CENTER);
 
         //mainFrame.revalidate();
         mainFrame.repaint();
         mainFrame.setVisible(true);
+    }
+
+    private void setColumnWidth() {
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+        TableColumn col1 = table.getColumnModel().getColumn(0);
+        TableColumn col2 = table.getColumnModel().getColumn(1);
+        TableColumn col3 = table.getColumnModel().getColumn(2);
+        col1.setPreferredWidth((int) (mainFrame.getWidth() * 0.8));
+        col2.setPreferredWidth((int) (mainFrame.getWidth() * 0.1));
+        col3.setPreferredWidth(mainFrame.getWidth() - col1.getPreferredWidth() - col2.getPreferredWidth() - 2);
     }
 }
